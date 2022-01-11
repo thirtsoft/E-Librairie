6271ab4f76290da97a6db48c166bffa5a928679f
thirtsoft <thirtsoft@gmail.com>
2022-01-11 00:46:53 +0000

    integrate profil separator pages

--- src/app/services/scategorie.service.ts
+++ src/app/services/scategorie.service.ts
@@ -16,9 +16,9 @@ const EXCEL_EXTENSION = '.xlsx';
 export class ScategorieService {
 
 
-  baseUrl_1 = environment.apiBaseUrl;
+ // baseUrl_1 = environment.apiBaseUrl;
 
-// baseUrl_1 = 'https://alamine-admin.herokuapp.com/gestionstock-alamine/v1';
+ baseUrl_1 = 'https://alamine-admin.herokuapp.com/gestionstock-alamine/v1';
 
 //  baseUrl_1 = "http://localhost:8080/Library-0.0.1-SNAPSHOT/gestionstock-alamine/v1";
 
